09379930f5e1cedece35f60ca58c5a7718e6b000
foio <syszhpe@gmail.com>
2016-05-19 14:47:27 +0800

    and simple

--- test.js
+++ test.js
@@ -3,4 +3,4 @@ console.log('git is free software');
 console.log('git is free GLP');
 console.log('git has a mutable index called stage');
 console.log('git tracks changes of files');
-console.log('creating a new branch quick');
+console.log('creating a new branch quick is quick and simple');
