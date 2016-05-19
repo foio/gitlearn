392e1b08d7993c5687aa7962f5c3e4bc8181b2ec
foio <syszhpe@gmail.com>
2016-05-19 14:41:51 +0800

    branch tset

--- test.js
+++ test.js
@@ -3,3 +3,4 @@ console.log('git is free software');
 console.log('git is free GLP');
 console.log('git has a mutable index called stage');
 console.log('git tracks changes of files');
+console.log('creating a new branch quick');
